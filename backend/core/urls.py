b4c03de6fabed192596f753485bf371ff2ded7d9
KhushiMulchandani <mulchandanikhushi829@gmail.com>
2026-08-03 03:54:43 +0530

    feat(backend): predict endpoint (stub data)

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OHLCVListView,FeatureListView,PriceListView,CompareListView,BacktestListView
+from .views import OHLCVListView,FeatureListView,PriceListView,CompareListView,BacktestListView,PredictStubView
 
 urlpatterns = [
     path('ohlcv/', OHLCVListView.as_view(), name='ohlcv-list'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path("price/", PriceListView.as_view(), name="price-list"),
     path("compare/", CompareListView.as_view(), name="compare-list"),
     path("backtest/", BacktestListView.as_view(), name="backtest-list"),
+    # Stage 7 Prediction Stub route
+    path("predict/", PredictStubView.as_view(), name="predict-stub"),
 ]
